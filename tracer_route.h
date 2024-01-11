6feb462c0d54f2a805287963b9ed5d35c7e22955
Shubhangi47 <50401971+shubhangi47@users.noreply.github.com>
2024-01-11 22:34:57 +0530

    Update tracer_route.h

--- tracer_route.h
+++ tracer_route.h
@@ -1,36 +1,59 @@
 #ifndef TRACER_ROUTE_H
 #define TRACER_ROUTE_H
 
-#include <iostream>
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
 #include <sys/types.h>
-#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <pthread.h>
 #include <poll.h>
-#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <sys/sem.h>
+#include <poll.h>
+#include <pthread.h>
+#include <sys/select.h>
+#include <sys/un.h>
+#include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
-#include <netinet/icmp6.h>
-#include <linux/errqueue.h>
 class Tracer_route
 {
 public:
-Tracer_route(std::string hostname);
-int createUDPsocket(int );
-int createICMPsocket();
-~Tracer_route(){
-
-}
-void pingUDP();
-
-
+    Tracer_route(const char *hostname);
+    int createICMPsocket();
+    int ICMPHop();
+    ~Tracer_route()
+    {
+        close(this->icmpsocket);
+    }
+    unsigned short csum(unsigned short *buf, int nwords)
+    {
+        unsigned long sum;
+        for (sum = 0; nwords > 0; nwords--)
+            sum += *buf++;
+        sum = (sum >> 16) + (sum & 0xffff);
+        sum += (sum >> 16);
+        return ~sum;
+    }
 
 private:
-int destport = 33435;
-std::string hostname;
-int udpSocket;
-struct addrinfo exp, *connection;
+    int destport = 33435;
+    const char *hostname;
+    int icmpsocket;
+    struct ip *ip_hdr;
+    int hop = 0;
+    char buf[4096];
+    struct sockaddr_in addr;
 };
 
 #endif
